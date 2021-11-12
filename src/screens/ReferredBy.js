474c085d0763a9b9dd3499029b9834bf68290c94
mickytroxxy <mickylyricalkartel@gmail.com>
2021-11-12 15:06:35 +0200

    changes

--- src/screens/ReferredBy.js
+++ src/screens/ReferredBy.js
@@ -3,11 +3,15 @@ import Box from '@mui/material/Box';
 import { AppContext } from '../context/AppContext';
 import { useParams } from 'react-router-dom'
 export default function ReferredBy() {
-    const {setDialogData, mobileView, setReferredBy, navigate} = React.useContext(AppContext);
+    const {setDialogData, mobileView, setReferredBy, navigate, loggedUser} = React.useContext(AppContext);
     const { phoneNumber } = useParams();
     React.useEffect(()=>{
-        setReferredBy(phoneNumber);
-        !mobileView ? setDialogData({visible:true,title:'GET STARTED'}) : navigate("mobile",{page:'GET STARTED'}) 
+        if(!loggedUser){
+            setReferredBy(phoneNumber);
+            !mobileView ? setDialogData({visible:true,title:'GET STARTED'}) : navigate("mobile",{page:'GET STARTED'})
+        }else{
+            navigate("Dashboard");
+        } 
     },[phoneNumber])
     return (
         <Box textAlign="center"></Box>
