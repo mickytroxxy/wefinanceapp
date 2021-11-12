70737ec71b484da06bb725394923c2cc3cacfdb0
mickytroxxy <mickylyricalkartel@gmail.com>
2021-11-12 16:18:12 +0200

    changes

--- src/components/modal/Referrals.js
+++ src/components/modal/Referrals.js
@@ -10,13 +10,21 @@ export default function MakePayment(props) {
         navigator.clipboard.writeText("https://wefinancegroup.org/referredBy/"+loggedUser.phoneNumber);
         setToastData({visible:true,text:'Referral link copied!',severity:'success'});
     }
+    const withdraw_btn_clicked =()=>{
+        if(!mobileView){
+            setDialogData({visible:true,title:'WITHDRAW YOUR FUNDS',data:{isSuccess:false,amount:0}})
+        }else{
+            setDialogData({visible:false})
+            navigate("mobile",{page:'WITHDRAW YOUR FUNDS',data:{isSuccess:false,amount:0}})
+        }
+    }
     return (
         <>
             <Box textAlign='center'>
                 <h3 className="fontBold1">YOUR REFERRAL LINK</h3>
                 <Button className="fontBold"  onClick={copyText} style={{textTransform: 'lowercase'}}>https://wefinancegroup.org/referredBy/{loggedUser.phoneNumber}</Button>
                 <h1 className="fontBold1">{formatToCurrency(referralBalance)}</h1>
-                <Button onClick={()=> !mobileView ? setDialogData({visible:true,title:'WITHDRAW YOUR FUNDS',data:{isSuccess:false,amount:0}}) : navigate("mobile",{page:'WITHDRAW YOUR FUNDS',data:{isSuccess:false,amount:0}})} variant="contained" style={{backgroundColor:'green',color:'#fff'}}  component="label"startIcon={<AtmIcon style={{fill: "#fff"}}/>}>
+                <Button onClick={withdraw_btn_clicked} variant="contained" style={{backgroundColor:'green',color:'#fff'}}  component="label"startIcon={<AtmIcon style={{fill: "#fff"}}/>}>
                     <span className="fontBold">WITHDRAW</span>
                 </Button>
             </Box>
--- src/screens/ReferredBy.js
+++ src/screens/ReferredBy.js
@@ -3,20 +3,11 @@ import Box from '@mui/material/Box';
 import { AppContext } from '../context/AppContext';
 import { useParams } from 'react-router-dom'
 export default function ReferredBy() {
-    const {setDialogData, mobileView, setReferredBy, navigate, loggedUser} = React.useContext(AppContext);
+    const {setDialogData, mobileView, setReferredBy, navigate} = React.useContext(AppContext);
     const { phoneNumber } = useParams();
     React.useEffect(()=>{
-        if(!loggedUser){
-            setReferredBy(phoneNumber);
-            if(!mobileView){
-                setDialogData({visible:true,title:'GET STARTED'})
-            }else{
-                setDialogData({visible:false})
-                navigate("mobile",{page:'GET STARTED'})
-            }
-        }else{
-            navigate("Dashboard");
-        } 
+        setReferredBy(phoneNumber);
+        !mobileView ? setDialogData({visible:true,title:'GET STARTED'}) : navigate("mobile",{page:'GET STARTED'}) 
     },[phoneNumber])
     return (
         <Box textAlign="center"></Box>
